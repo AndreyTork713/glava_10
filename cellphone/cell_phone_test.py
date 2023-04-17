83f9ba337e785dd734ba001923df7f7f2b4fa69f
Andrey <tork713@gmail.com>
2023-04-17 12:03:45 +0700

    Написал cell_phone_test

---
+++ cellphone/cell_phone_test.py
@@ -0,0 +1,15 @@
+import cellphone
+
+def main():
+    man = input('Введите марку телефона: ')
+    mod = input('Введите модель телефона: ')
+    retail = float(input('Введите розничную цену телефона: '))
+
+    cell = cellphone.Cellphone(man, mod, retail)
+    print(f'Марка телефона: {cell.get_manufact()}')
+    print(f'Модель телефона: {cell.get_model()}')
+    print(f'Розничная цена телефона: {cell.get_retail_price()}')
+
+
+if __name__== '__main__':
+    main()

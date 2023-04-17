9d1a8be05a06151d2a95df76ee8553dbba4c1229
Andrew <tork713@gmail.com>
2023-04-17 21:01:26 +0700

    список телефонов

---
+++ cellphone/cell_phone_list.py
@@ -0,0 +1,39 @@
+# Программа создает 5 объектов Cellphone и сохраняет их в списке
+import cellphone
+
+
+def main():
+
+    # Получить список объектов Cellphone
+    phones = make_list()
+
+    # Показать данные в списке
+    print('Вот введенные вами данные: ')
+    display_list(phones)
+
+
+def make_list():
+    phone_list = []
+
+    for count in range(1, 6):
+        print(f'Номер телефона: {count}')
+        man = input('Введите производителя телефона: ')
+        mod = input('Введите модель телефона: ')
+        retail = int(input('Введите розничную цену телефона: '))
+        print()
+
+        phone = cellphone.Cellphone(man, mod, retail)
+        phone_list.append(phone)
+    return phone_list
+
+
+def display_list(phone_list):
+    for item in phone_list:
+        print(item.get_manufact())
+        print(item.get_model())
+        print(item.get_retail_price())
+        print()
+
+
+if __name__ == '__main__':
+    main()
--- cellphone/cell_phone_test.py
+++ cellphone/cell_phone_test.py
@@ -8,7 +8,7 @@ def main():
     cell = cellphone.Cellphone(man, mod, retail)
     print(f'Марка телефона: {cell.get_manufact()}')
     print(f'Модель телефона: {cell.get_model()}')
-    print(f'Розничная цена телефона: {cell.get_retail_price()}')
+    print(f'Розничная цена телефона: {cell.get_retail_price()} руб.')
 
 
 if __name__== '__main__':
